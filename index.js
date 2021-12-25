6299a1b55ee0e5ff937b867ebd47a79630bf5324
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-12-26 00:32:08 +0100

    Setup app listen and coinbase client, charge and event.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// Imports
 const express = require('express');
 const coinbase = require('coinbase-commerce-node');
 const dotenv = require('dotenv').config();
+
+// App setup
+const app = express();
+let server = app.listen(process.env.PORT, () => {
+	console.log('Listening on port ' + process.env.PORT);
+});
+
+// Coinbase setup
+const Client = coinbase.Client;
+Client.init(process.env.API_KEY);
+const Charge = coinbase.resources.Charge;
+const Event = coinbase.resources.Event;
