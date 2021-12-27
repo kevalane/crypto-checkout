bf51ad6ca338c809431baf24909805a567a2b406
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-12-27 21:28:02 +0100

    Added body parser.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Imports
 const express = require('express');
 const coinbase = require('coinbase-commerce-node');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 
 // App setup
@@ -8,18 +9,19 @@ const app = express();
 let server = app.listen(process.env.PORT, () => {
 	console.log('Listening on port ' + process.env.PORT);
 });
+app.use(bodyParser.json());
 
 // Coinbase setup
 const Client = coinbase.Client;
 Client.init(process.env.API_KEY);
 const Charge = coinbase.resources.Charge;
 
-app.get('/charge', (req, res) => {
+app.post('/charge', (req, res) => {
 	let chargeData = {
-		name: 'Kevin Rasmusson',
-		description: 'Test description',
+		name: req.body.name,
+		description: req.body.description,
 		local_price: {
-			amount: 100,
+			amount: req.body.amount,
 			currency: 'USD'
 		},
 		pricing_type: 'fixed_price'
@@ -29,26 +31,30 @@ app.get('/charge', (req, res) => {
 		if (err) {
 			res.status(400).send({message: err.message});
 		} else {
-			// response['id']
-			// response['hosted_url']
 			res.status(200).send(response);
 		}
 	});
 });
 
-app.get('/status', (req, res) => {
-	let id = '4c3acb58-60f9-40f2-9ee1-83b2ad3f2f35';
+app.post('/status', (req, res) => {
+	let id = req.body.id;
 	Charge.retrieve(id, (err, charge) => {
+		console.log(charge);
 		if(charge['timeline'][0]['status'] == 'NEW') {
-			// created, nothing happening
-		} else if (charge['timeline'][1]['status'] == 'PEDNING') {
-			// pending, payment has been seen
-		} else if (charge['timeline'][1]['status'] == 'EXPIRED') {
-			// the hour has passed, expired charge
-		} else if (charge['timeline'][2]['status'] == 'COMPLETED') {
-			// payment completed
+			try {
+				if (charge['timeline'][1]['status'] == 'PEDNING' && charge['timeline'].length == 2) {
+					return res.status(200).send({message: 'Payment pending, awaiting confirmations.'});
+				} else if (charge['timeline'][1]['status'] == 'EXPIRED') {
+					return res.status(400).send({message: 'Payment expired'});
+				} else if(charge['timeline'][2]['status'] == 'COMPLETED') {
+					return res.status(200).send({message: 'Payment completed.'});
+				}
+			} catch(err) {
+				return res.status(200).send({message: 'No payment detected'});
+			}
+		} else {
+			return res.status(400).send({message: 'Charge not found.'});
 		}
-		res.send(charge);
 	});
 });
 
