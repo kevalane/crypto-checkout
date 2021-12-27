00beb23c2137e671cc6e049d517316af4956134f
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-12-27 18:11:44 +0100

    Implemented create charge route.

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ const Client = coinbase.Client;
 Client.init(process.env.API_KEY);
 const Charge = coinbase.resources.Charge;
 const Event = coinbase.resources.Event;
+
+app.get('/charge', (req, res) => {
+	let chargeData = {
+		name: 'Kevin Rasmusson',
+		description: 'Test description',
+		local_price: {
+			amount: 100,
+			currency: 'USD'
+		},
+		pricing_type: 'fixed_price'
+	}
+
+	Charge.create(chargeData, (err, response) => {
+		if (err) {
+			res.status(400).send({message: err.message});
+		} else {
+			res.status(200).send(response);
+		}
+	});
+});
+
